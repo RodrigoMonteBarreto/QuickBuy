97d3a2b4f1bc21608915cf1f7e38d0eb3d0b1d39
rodrigo barreto <rodrigo_.barreto@hotmail.com>
2019-05-13 21:41:11 -0300

    Atualização

--- QuickBuy.Dominio/Entidades/FormaPagamento.cs
+++ QuickBuy.Dominio/Entidades/FormaPagamento.cs
@@ -2,5 +2,10 @@
 {
     public class FormaPagamento
     {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
     }
 }
