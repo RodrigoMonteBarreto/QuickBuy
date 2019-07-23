37bce7de8ec4761beadd56d9eebf9cc61b48bdfe
rodrigo barreto <rodrigo_.barreto@hotmail.com>
2019-07-23 10:11:53 -0300

    Atualização

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,26 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../nav-menu/model/usuario";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls:["./login.component.css"]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+   
   public usuario;
+  public returnUrl: string;
 
-  constructor(private router: Router) {
-    this.usuario = new Usuario();
+  constructor(private router: Router, private activateRouter: ActivatedRoute) {  
+   
   }
 
+  ngOnInit(): void {
+    this.returnUrl = this.activateRouter.snapshot.queryParams['returnUrl'];
+    this.usuario = new Usuario();
+  }
 
   entrar() {
 
     if (this.usuario.email == "rodrigobarreto@.com.br" && this.usuario.senha == "abc") {
 
       sessionStorage.setItem("usuario-autenticado", "1");
-      this.router.navigate(['/']);
+      this.router.navigate([this.returnUrl]);
 
     }
 
