e56df2ca50eb37304021bf0b31391a2182cda14e
rodrigo barreto <rodrigo_.barreto@hotmail.com>
2019-07-31 17:28:00 -0300

    Alterações no projeto

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -14,6 +14,7 @@ export class LoginComponent implements OnInit {
   public usuario;
   public returnUrl: string;
   public mensagem: string;
+  private ativar_spinner: boolean;
 
   constructor(private router: Router, private activateRouter: ActivatedRoute, private usuarioServico: UsuarioServico) {  
    
@@ -25,7 +26,7 @@ export class LoginComponent implements OnInit {
   }
 
   entrar() {
-
+    this.ativar_spinner = true;
     this.usuarioServico.verficarUsuario(this.usuario)
       .subscribe(
 
@@ -44,6 +45,7 @@ export class LoginComponent implements OnInit {
         err => {
           console.log(err.error);
           this.mensagem = err.error;
+          this.ativar_spinner = false;
         }
 
       );
